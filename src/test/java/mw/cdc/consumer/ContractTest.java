26a6b935ab81a8cb24d071812780e4695b940bee
mariusz.wojcik <mariusz.wojcik@asseco.pl>
2020-12-21 08:57:02 +0100

    contract changed test

--- src/test/java/mw/cdc/consumer/ContractTest.java
+++ src/test/java/mw/cdc/consumer/ContractTest.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ContractTest {
-    
+
+    public static final String BASEURL = "http://192.168.0.116:9876";
+
     @DisplayName("Should return Yes when service is alive")
     @Test 
     void shouldReturnYesWhenServiceIsAlive() {
         //RestAssured.given().when().get("http://www.google.com").then().statusCode(200);
-        RestAssured.given().when().param("question","Are you alive?").get("http://192.168.0.116:9876/askme").then().statusCode(200).body(
+        RestAssured.given().when().param("question","Are you alive?").param("foo","foo").get(BASEURL + "/askme").then().statusCode(200).body(
             Matchers.equalTo("Yes"));
      }
 
